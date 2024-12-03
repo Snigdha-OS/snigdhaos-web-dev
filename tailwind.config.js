e18e5ca41c7768d6c4c4d00c9511bb7a5d49decd
Eshan Roy <m.eshanized@gmail.com>
2024-12-03 19:54:23 +0530

    IDK

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    './src/**/*.{js,ts,jsx,tsx}',
-    // Add paths to other files that use Tailwind
-  ],
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
